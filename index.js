300a3aced20223222207fd28c1ba69e2084372b9
Omid Kayhani <o.kayhani@gmail.com>
2023-08-22 15:57:53 +0000

    solve assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-class Formatter {
-  //add static methods here
+class Formatter { 
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(sentence) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    let wordsArray = sentence.split(" ");
+    for (let n = 0; n < wordsArray.length; n++) {
+      if (n === 0) {
+        result.push(this.capitalize(wordsArray[n]))
+      } else {
+        if (exceptions.includes(wordsArray[n])) {
+          result.push(wordsArray[n])
+        } else {
+          result.push(this.capitalize(wordsArray[n]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
 }
